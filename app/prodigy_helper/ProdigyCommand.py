889f96fe0157954e556d90c046117d2e2158e45d
BobWatson <bob@nearimpossible.com>
2021-09-08 04:36:20 +0000

    1. Cleaned up unused files 2. Increased wait for pkill of prodigy

--- app/prodigy_helper/ProdigyCommand.py
+++ app/prodigy_helper/ProdigyCommand.py
@@ -64,8 +64,9 @@ class ProdigyCommand ():
         
     def stop(self):
         self.event.set()
-        time.sleep(20)
+        time.sleep(60)
         if self.running is True and self.event.is_set():
+            print("Timeout expired, killing process...")
             os.system("killall prodigy")
         
     def _annotate_manual(self,output_file):
