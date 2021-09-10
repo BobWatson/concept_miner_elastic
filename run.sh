30cf5e854737a85ca7911f36ec8f0aa9d4ce788c
BobWatson <bob@nearimpossible.com>
2021-09-11 07:20:44 +1000

    Updated to not require VS Code

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+export PATH=/home/concept_miner/.local/bin:$PATH
+cd /concept_miner/app
+
+if ! [ "${PRODIGY_KEY}" = "" ]
+then
+    mkdir /tmp/build
+    envsubst < ../build/requirements.txt > /tmp/build/requirements.txt
+    pip3 --disable-pip-version-check install -r /tmp/build/requirements.txt
+else
+    "Fail"
+fi
+
+if [ "${DEBUG}" = "true" ]
+then
+    sleep infinity
+else
+    /usr/local/bin/python ./app.py
+fi
