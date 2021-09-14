deb33a55ada162cde621120da880f2863d639b19
BobWatson <bob@nearimpossible.com>
2021-09-15 07:14:22 +1000

    Small change to trigger black

--- app/web/web.py
+++ app/web/web.py
@@ -26,64 +26,80 @@ app = Flask(__name__)
 api = Api(app)
 
 er = eventReceiver.withEmitter(ee)
-api.add_resource(er, '/event/<string:event_id>')
+api.add_resource(er, "/event/<string:event_id>")
 
 app.jinja_env.auto_reload = True
-app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.config['MAX_CONTENT_LENGTH'] = 1600 * 1024 * 1024
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["MAX_CONTENT_LENGTH"] = 1600 * 1024 * 1024
 
 ## -- Routes -- ##
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/home.html')
+
+@app.route("/home.html")
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
 
-@app.route('/training-log.html')
+
+@app.route("/training-log.html")
 def training_log():
     conv = Ansi2HTMLConverter()
-    with open(os.path.abspath(f"{cfg['folders']['output']}/prodigy.train.log"),"r") as f:
+    with open(
+        os.path.abspath(f"{cfg['folders']['output']}/prodigy.train.log"), "r"
+    ) as f:
         logdata = f.read()
-        
-    with open(os.path.abspath(f"{cfg['folders']['output']}/prodigy.train-curve.log"),"r") as f:
-        curvedata = f.read()
-        
-    html_log = conv.convert(logdata.replace("`","'"))
-    html_curve_log = conv.convert(curvedata.replace("`","'"))
-    
-    return render_template('training-log.html',logdata=html_log,curvedata=html_curve_log)
 
-@app.route('/upload', methods = ['POST'])
+    with open(
+        os.path.abspath(f"{cfg['folders']['output']}/prodigy.train-curve.log"), "r"
+    ) as f:
+        curvedata = f.read()
+
+    html_log = conv.convert(logdata.replace("`", "'"))
+    html_curve_log = conv.convert(curvedata.replace("`", "'"))
+
+    return render_template(
+        "training-log.html", logdata=html_log, curvedata=html_curve_log
+    )
+
+
+@app.route("/upload", methods=["POST"])
 def upload_file():
-    if request.method == 'POST':
-        for file in request.files.getlist('files'):
-            file_name = os.path.join(cfg['folders']['watch_folder'],secure_filename(file.filename))
+    if request.method == "POST":
+        for file in request.files.getlist("files"):
+            file_name = os.path.join(
+                cfg["folders"]["watch_folder"], secure_filename(file.filename)
+            )
             logging.info(f"Received {file_name}")
             file.save(file_name)
         return json.dumps({"status": "success"})
     else:
         return json.dumps({"status": "failure"})
 
+
 ## --- Done with routes --- ##
 
-dw = directoryWatcher(cfg['folders']['watch_folder'])
+dw = directoryWatcher(cfg["folders"]["watch_folder"])
+
 
 @ee.on("directory_watcher.start")
 def run_directory_watcher():
-    
+
     print("Running Directory Watcher")
-    
-    dw.add_handler("pdf",PDFExtractor.getContent)
-    dw.add_handler("txt",TextExtractor.getContent)
-    
+
+    dw.add_handler("pdf", PDFExtractor.getContent)
+    dw.add_handler("txt", TextExtractor.getContent)
+
     dw.run(30)
+
 
 @ee.on("directory_watcher.stop")
 def stop_directory_watcher():
     dw.stop()
+
 
 prodigy_running = False
 app_should_loop = True
@@ -91,83 +107,89 @@ can_halt = False
 
 prodigy = ProdigyCommand()
 
+
 @ee.on("prodigy.start")
 def run_prodigy():
     p = threading.Thread(target=_run_prodigy)
     p.start()
 
+
 def _run_prodigy():
     global prodigy_running
     global app_should_loop
-    
+
     if prodigy_running:
         print("Prodigy already running")
         return
-    
+
     print("Running prodigy...")
-    
-    jsonl = ElasticManager.build_jsonl(cfg['prodigy']['batch_size'])
-        
-    output_file = os.path.join(cfg['folders']['output'], "data.jsonl")
-    
-    with open(output_file, 'w') as f:
+
+    jsonl = ElasticManager.build_jsonl(cfg["prodigy"]["batch_size"])
+
+    output_file = os.path.join(cfg["folders"]["output"], "data.jsonl")
+
+    with open(output_file, "w") as f:
         f.write(jsonl)
         f.close()
-    
+
     prodigy_running = True
     prodigy.annotate_manual(output_file=output_file)
-    
+
     while prodigy_running:
         time.sleep(5)
-        
-    prodigy.db_out(output_dir=cfg['folders']['output'])
+
+    prodigy.db_out(output_dir=cfg["folders"]["output"])
     ee.emit("prodigy.db_written")
-    
+
     if app_should_loop:
-        
+
         print("Training...")
-    
-        prodigy.train(output_dir=cfg['folders']['output'])
-        prodigy.train_curve(output_dir=cfg['folders']['output'])
-        
+
+        prodigy.train(output_dir=cfg["folders"]["output"])
+        prodigy.train_curve(output_dir=cfg["folders"]["output"])
+
         ee.emit("prodigy.start")
-        
+
     else:
         print("Prodigy done.")
         ee.emit("prodigy.done")
 
+
 @ee.on("prodigy.stop")
 def stop_prodigy():
     global prodigy_running
-    
+
     prodigy.stop()
     prodigy_running = False
+
 
 @ee.on("app.halt")
 def halt_app():
     global app_should_loop
     global can_halt
     app_should_loop = False
-    
+
     ee.emit("prodigy.stop")
     ee.emit("directory_watcher.stop")
-    
+
     while not can_halt:
         time.sleep(5)
-    
+
+
 @ee.on("prodigy.done")
 def _halt_app():
     global can_halt
-    print ("Prodigy stopped. App can exit.")
+    print("Prodigy stopped. App can exit.")
     can_halt = True
-    
+
+
 @ee.on("prodigy.db_written")
 def read_annotations_into_elastic():
     em = ElasticManager(elastic_index=cfg["elastic"]["annotation_index"])
-    annotations_folder = cfg['folders']['output']+"/annotations"
-    fileList = [x for x in Path(annotations_folder).rglob('*.jsonl')]
+    annotations_folder = cfg["folders"]["output"] + "/annotations"
+    fileList = [x for x in Path(annotations_folder).rglob("*.jsonl")]
     for file_name in fileList:
-        file = open(file_name, 'r')
+        file = open(file_name, "r")
         lines = file.readlines()
         for line in lines:
             line_dict = json.loads(line)
@@ -179,27 +201,29 @@ def read_annotations_into_elastic():
                 id = line_dict["meta"]["id"]
             except:
                 id = "<err>"
-                
+
             em.update(line_dict, id)
+
 
 @ee.on("prodigy.write_annotations_to_elastic")
 def write_annotations_to_elastic():
-    prodigy.db_out(output_dir=cfg['folders']['output'])
+    prodigy.db_out(output_dir=cfg["folders"]["output"])
     ee.emit("prodigy.db_written")
+
 
 @app.before_first_request
 def main():
     global prodigy_running
     global app_should_loop
     global can_halt
-    
+
     prodigy_running = False
     app_should_loop = True
     can_halt = False
-    
-    atexit.register(ee.emit,'app.halt')
-    
+
+    atexit.register(ee.emit, "app.halt")
+
     ee.emit("directory_watcher.start")
     ee.emit("prodigy.start")
-    
+
     print("Ready.")
